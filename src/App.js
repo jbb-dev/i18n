7b3b9eee1b07a6744b2d1cd01d416c9640b5fcf6
Jean-Baptiste Bouillat <jbb@mbpdejebaptiste.home>
2023-04-14 12:56:40 +0200

    basic install

--- src/App.js
+++ src/App.js
@@ -1,13 +1,38 @@
 import logo from './logo.svg';
 import './App.css';
+import { useTranslation, Trans } from 'react-i18next';
+import { useState } from 'react';
+import i18next from 'i18next';
 
 function App() {
+
+  const [lang, setLang] = useState('en');
+
+  const { t, i18n } = useTranslation();
+
+  const handleLanguage = () => {
+    if (lang === 'en')
+    {
+      setLang('de');
+      i18n.changeLanguage('de')
+    }
+    else
+    {
+      setLang('en');
+      i18n.changeLanguage('en')
+    };
+  };
+
   return (
     <div className="App">
       <header className="App-header">
+        <button onClick={handleLanguage}>Change Language</button>
+        <button onClick={() => console.log(i18next.resolvedLanguage)}>Current Language</button>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <Trans i18nKey="description.part1">
+            Edit <code>src/App.js</code> and save to reload.
+          </Trans>
         </p>
         <a
           className="App-link"
@@ -15,7 +40,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          {t('description.part2')}
         </a>
       </header>
     </div>
---
+++ src/i18n.js
@@ -0,0 +1,36 @@
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
+
+i18n
+  // pass the i18n instance to react-i18next.
+  .use(initReactI18next)
+  // init i18next
+  // for all options read: https://www.i18next.com/overview/configuration-options
+  .init({
+    debug: true,
+    fallbackLng: 'en',
+    interpolation: {
+      escapeValue: false, // not needed for react as it escapes by default
+    },
+    resources: {
+        en: {
+            translation: {
+                description: {
+                    part1: 'Edit <1>src/App.js</1> and save to reload.',
+                    part2: 'Learn React'
+                }
+            // here we will place our translations...
+            }
+        },
+        de: {
+            translation: {
+            description: {
+                part1: 'Ändere <1>src/App.js</1> und speichere um neu zu laden.',
+                part2: 'Lerne React'
+            }
+            }
+      }
+    }
+  });
+
+export default i18n;
